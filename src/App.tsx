a06751b7a97aa03763a57bab128a23533eb6a1ee
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2023-06-02 18:01:57 +0300

    инициализация, функциональность

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useState } from "react";
+import "./App.css";
+import AddPizzaForm from "./components/AddPizzaForm";
+import Pizza from "./models/Pizza";
+import DisplayPizzas from "./components/DisplayPizzas";
+
+const App: FC = () => {
+  const [pizzaList, setPizzaList] = useState<Pizza[]>([]);
+  const addPizza = (newPizza: Pizza) => {
+    setPizzaList([...pizzaList, newPizza]);
+  };
+  const updatePizza = (newPizza: Pizza) => {
+    setPizzaList(
+      pizzaList.map((pizza) => 
+      (pizza.id === newPizza.id ? newPizza : pizza))
+    );
+  };
+  const deletePizza = (id: number) => {
+    const newPizzasList = pizzaList.filter(pizza => pizza.id !== id);
+    setPizzaList(newPizzasList);
+
+  }
+
+  return (
+    <div className="App">
+      <div className="wrap">
+        <span className="heading">Наша пиццерия</span>
+        <AddPizzaForm addPizza={addPizza} />
+        <DisplayPizzas pizzaList={pizzaList} updatePizza = {updatePizza}
+        deletePizza ={deletePizza}/>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/DisplayPizzas.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import Pizza from "../models/Pizza";
+import SinglePizza from "./SinglePizza";
+
+interface DisplayPizzasProps {
+  pizzaList: Pizza[];
+  updatePizza: (newPizza: Pizza) => void;
+  deletePizza: (id: number) => void;
+}
+const DisplayPizzas: FC<DisplayPizzasProps> = ({
+  pizzaList,
+  updatePizza,
+  deletePizza,
+}) => {
+  return (
+    <div className="container">
+      {pizzaList.map((pizza) => {
+        return (
+          <SinglePizza
+            key={pizza.id}
+            pizza={pizza}
+            updatePizza={updatePizza}
+            deletePizza={deletePizza}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default DisplayPizzas;
